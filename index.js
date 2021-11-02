bb1b734062747e114fc85a8902ce808302e7f346
John DeFeo <jackd3243@gmail.com>
2021-11-02 19:25:40 -0400

    commit tree 28df258ef98e5771d5ee4d7f8a317b2ba545c8a0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function  
+saturdayFun(activity= "roller-skate") {
+    if (activity) {
+       return `This Saturday, I want to ${activity}!`
+    }
+};
+function mondayWork(workspace="go to the office"){
+    if (workspace) {
+        return `This Monday, I will ${workspace}.`
+    }
+};
+function wrapAdjective(adjective= "*"){
+    return function(parameter= "special") {
+            return `You are ${adjective}${parameter}${adjective}!`
+            
+    }
+    
+}
